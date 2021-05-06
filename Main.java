6c7c96b38494ec45773b57edd578593d5a190aaf
sakthivino <sathya.cs17@bitsathy.ac.in>
2021-05-06 13:13:20 +0530

    My First Hello World program

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.*;
+
+class Main 
+
+	{
+		public static void main(String args[])
+		{
+		   
+			System.out.print("hai");
+			}
+	}
